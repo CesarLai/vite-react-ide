bd4d1efab292a5ebd8aba00beaf0c77b7ee1b5ac
Cesar Lai <git@cesarlai.com>
2022-04-26 23:29:13 +0800

    feat: update dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,26 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import * as path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
   return {
+    mode,
     plugins: [react()],
-    publicDir: "public",
-  };
+    publicDir: 'public',
+    resolve: {
+      extensions: ['.ts', '.tsx', '.json'],
+      alias: {
+        '@': path.resolve(__dirname, 'src')
+      }
+    },
+    css: {
+      modules: {
+        scopeBehaviour: 'local',
+        hashPrefix: 'cl',
+        generateScopedName: '[name]__[local]___[hash:base64:5]',
+        localsConvention: 'camelCaseOnly'
+      }
+    }
+  }
 })
